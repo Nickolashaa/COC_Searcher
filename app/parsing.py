5197cc4de0002c0acd16a83ba83c59ecf1b20f4c
Nickolashaa <gracevnikolaj220@gmail.com>
2025-02-20 01:22:59 +0300

    update for windows

--- app/parsing.py
+++ app/parsing.py
@@ -3,26 +3,38 @@ import pyaudio
 import wave
 import time
 import os
+import sys
 from paddleocr import PaddleOCR
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 
 class ScreenReader:
     def __init__(self):
         os.system("cd platform-tools && adb connect 127.0.0.1:5555")
         self.ocr = PaddleOCR(lang='en', show_log=False)
-    
+
     def read(self, name):
-        result = self.ocr.ocr(name, cls=False)
+        result = self.ocr.ocr(resource_path(name), cls=False)
         if result[-1] is None:
             return 0
         num = result[-1][-1][-1][0]
         num = int(''.join([c for c in num if c.isdigit()]))
         return num
-        
+
     def screen(self, region: tuple, coords: list, names: list):
-        os.system("cd platform-tools && adb -s 127.0.0.1:5555 shell screencap -p /sdcard/screen.png")
-        os.system("cd platform-tools && adb -s 127.0.0.1:5555 pull /sdcard/screen.png")
-        img = Image.open('platform-tools/screen.png')
+        os.system(
+            "cd platform-tools && adb -s 127.0.0.1:5555 shell screencap -p /sdcard/screen.png")
+        os.system(
+            "cd platform-tools && adb -s 127.0.0.1:5555 pull /sdcard/screen.png")
+        img = Image.open(resource_path('platform-tools\\screen.png'))
         img = img.crop(region)
         for i in range(3):
             crop_box = (
@@ -32,15 +44,15 @@ class ScreenReader:
                 coords[i][1] + coords[i][3]
             )
             cropped_img = img.crop(crop_box)
-            cropped_img.save(names[i])
-                
+            cropped_img.save(resource_path(names[i]))
+
     def play_sound(self):
-        wf = wave.open("app/sfx/sound.wav", "rb")
+        wf = wave.open(resource_path("app\\sfx\\sound.wav"), "rb")
         p = pyaudio.PyAudio()
         stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True)
+                        channels=wf.getnchannels(),
+                        rate=wf.getframerate(),
+                        output=True)
         data = wf.readframes(1024)
         while data:
             stream.write(data)
@@ -48,11 +60,12 @@ class ScreenReader:
         stream.stop_stream()
         stream.close()
         p.terminate()
-        
+
     def next(self):
-        os.system("cd platform-tools && adb -s 127.0.0.1:5555 shell input tap 1500 675")
+        os.system(
+            "cd platform-tools && adb -s 127.0.0.1:5555 shell input tap 1500 675")
         time.sleep(1.5)
-    
+
     def reload(self):
         os.system("cd platform-tools && adb kill-server")
         os.system("cd platform-tools && adb start-server")
@@ -65,13 +78,15 @@ def parsing(gold, elixir, dark_elixir, main_self):
         main_self.reader.screen(
             region=(80, 120, 230, 255),
             coords=[(0, 0, 150, 40), (0, 45, 150, 40), (0, 90, 150, 40)],
-            names=['app/parse_pic/gold.png', 'app/parse_pic/elixir.png', 'app/parse_pic/dark_elixir.png']
+            names=[resource_path('app\\parse_pic\\gold.png'),
+                   resource_path('app\\parse_pic\\elixir.png'),
+                   resource_path('app\\parse_pic\\dark_elixir.png')]
         )
         print(1)
-        current_gold = main_self.reader.read('app/parse_pic/gold.png')
-        current_elixir = main_self.reader.read('app/parse_pic/elixir.png')
-        current_dark_elixir = main_self.reader.read('app/parse_pic/dark_elixir.png')
-        
+        current_gold = main_self.reader.read('app\\parse_pic\\gold.png')
+        current_elixir = main_self.reader.read('app\\parse_pic\\elixir.png')
+        current_dark_elixir = main_self.reader.read('app\\parse_pic\\dark_elixir.png')
+
         print(2)
         if not current_gold or not current_elixir or not current_dark_elixir:
             time.sleep(2.5)
